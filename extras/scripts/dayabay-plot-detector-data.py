866f1ab3ced37bc24e6a38841b1715da8551cade
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-09-11 16:12:25 +0300

    chore: review lines

--- extras/scripts/dayabay-plot-detector-data.py
+++ extras/scripts/dayabay-plot-detector-data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# TODO: review
 
 from __future__ import annotations
 
--- src/dayabay_model_official/model_dayabay.py
+++ src/dayabay_model_official/model_dayabay.py
@@ -1,3 +1,4 @@
+# TODO: review
 from __future__ import annotations
 
 from collections.abc import Mapping, Sequence
