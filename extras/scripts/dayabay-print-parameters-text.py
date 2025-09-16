1042cf66db5b34031431f805dbb717b26cc734fa
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-09-16 12:48:44 +0300

    chore: cleaning

--- extras/scripts/dayabay-print-parameters-text.py
+++ extras/scripts/dayabay-print-parameters-text.py
@@ -9,16 +9,10 @@ $ ./extras/scripts/dayabay-print-parameters-text.py output/parameters.txt
 from __future__ import annotations
 
 from argparse import Namespace
-from typing import TYPE_CHECKING
 
 from dag_modelling.tools.logger import set_verbosity
-from dag_modelling.tools.save_records import save_records
 
 from dayabay_model_official import model_dayabay
-
-if TYPE_CHECKING:
-    from collections.abc import Sequence
-    from typing import Any
 
 
 def main(opts: Namespace) -> None:
@@ -31,21 +25,10 @@ def main(opts: Namespace) -> None:
     storage["parameters.all"].to_text_file(opts.output)
 
 
-def save_summary(model: Any, filenames: Sequence[str]):
-    data = {}
-    try:
-        for period in ["total", "6AD", "8AD", "7AD"]:
-            data[period] = model.make_summary_table(period=period)
-    except AttributeError:
-        return
-
-    save_records(data, filenames, tsv_allow_no_key=True, to_records_kwargs={"index": False})
-
-
 if __name__ == "__main__":
     from argparse import ArgumentParser
 
-    parser = ArgumentParser()
+    parser = ArgumentParser(description="Print all the parameters to a text file")
     parser.add_argument("-v", "--verbose", default=1, action="count", help="verbosity level")
     parser.add_argument("output", help="file to print parameters to", metavar="file")
     parser.add_argument(
