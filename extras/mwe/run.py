8d2beb99e0b6e96d83f3a6676e2c19bac79f63e4
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-11-13 17:53:34 +0300

    chore: make mwe scripts executable

--- extras/mwe/run.py
+++ extras/mwe/run.py
@@ -1,4 +1,4 @@
 from dayabay_model_official import model_dayabay
 
 model = model_dayabay()
-print(model.storage["outputs.statistic.full.covmat.chi2n"].data)
+print(model.storage["outputs.statistic.full.covmat.chi2p"].data)
