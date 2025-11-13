5ea0d92c1602a232f0d6e377a47c30fe68021b65
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-11-13 18:02:04 +0300

    chore: update mwe scripts

--- extras/mwe/run.py
+++ extras/mwe/run.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python
+
 from dayabay_model_official import model_dayabay
 
 model = model_dayabay()
-print(model.storage["outputs.statistic.full.covmat.chi2p"].data)
+print("χ² CNP value:", model.storage["outputs.statistic.full.covmat.chi2p"].data)
