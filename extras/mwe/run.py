db6148c998669131eb70c1065f3ec94752fa77fe
Maxim Gonchar <maxim.mg.gonchar@gmail.com>
2025-11-14 12:03:17 +0300

    minor

--- extras/mwe/run.py
+++ extras/mwe/run.py
@@ -3,4 +3,4 @@
 from dayabay_model_official import model_dayabay
 
 model = model_dayabay()
-print("χ² CNP value:", model.storage["outputs.statistic.full.covmat.chi2p"].data)
+print("χ² CNP value:", model.storage["outputs.statistic.full.covmat.chi2cnp"].data)
